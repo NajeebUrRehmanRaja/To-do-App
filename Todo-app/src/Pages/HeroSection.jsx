20e0af5e19d21814fce5ec1e5d9d84cfbe6930e9
NajeebUrRehmanRaja <najeebraja22.2@gmail.com>
2025-04-19 22:44:31 +0500

    Implement input handling and update toast message in HeroSection

--- Todo-app/src/Pages/HeroSection.jsx
+++ Todo-app/src/Pages/HeroSection.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router';
 import { toast } from "react-toastify";
 
 const HeroSection = () => {
+  const [inputValue, setInputValue] = useState("");
   const showToast = () => {
-     toast.success("Operation successful!", {
+     toast.success("New Task Added!", {
        hideProgressBar: true,
        autoClose: 2000, // optional, sets how long toast stays visible
        position: "bottom-right", // optional, sets the position of the toast
@@ -16,9 +17,21 @@ const HeroSection = () => {
       <h1 className="text-4xl font-bold mb-4">Create your Daily Tasks!</h1>
       <div>
         {/* <label htmlFor="taskInput" className="text-lg font-semibold mr-2"> Task: </label> */}
-        <input type="text" id="taskInput" className="border border-gray-300 rounded px-10 py-3" placeholder='Add New Task' /> 
-        <button className="ml-2 bg-blue-500 text-white px-10 py-3 rounded cursor-pointer hover:bg-blue-600" onClick={showToast}
-        >Add Task</button>
+        <input
+          type="text"
+          id="taskInput"
+          className="border border-gray-300 rounded px-10 py-3"
+          placeholder="Add New Task"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button
+          className={`ml-2 bg-blue-500 text-white px-10 py-3 rounded cursor-pointer hover:bg-blue-600 transition duration-300 ${!inputValue.trim() ? "opacity-50 cursor-not-allowed" : ""}`}
+          onClick={showToast}
+          disabled={!inputValue.trim()}
+        >
+          Add Task
+        </button>
         {/* <Link to="/mylists" className="cursor-pointer text-sm underline text-blue-600 flex ml-105 mt-1 ">MyLists</Link> */}
       </div>
     </div>
